b6eb8a08dee9935d4b2b18a6636f9e0e4517eff1
Excel619 <excel8392@gmail.com>
2023-05-18 16:21:02 -0600

    Finished adding modules, added libs, updated gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,30 @@ group = "com.runicrealms"
 
 val rootFolder = rootProject.buildDir
 allprojects {
+//    println("allproject name: " + this.name)
+//    println("allproject subprojects: " + this.childProjects.entries)
     buildDir = (parent?.buildDir ?: rootFolder).resolve(name)
+//    if (this.name == "Projects") {
+//        this.childProjects.entries.forEach {
+//            println("SETTING VERSION FOR: " + it.key)
+//            //it.value.tasks.create("wrapper")
+///*            it.value.tasks.wrapper {
+//                gradleVersion = "7.6.1"
+//            }*/
+//        }
+//    }
+
 }
+
+//subprojects {
+//    println("NAME: " + this.name)
+//    buildDir = (parent?.buildDir ?: rootFolder).resolve(name)
+//    tasks.wrapper {
+//        gradleVersion = "7.6.1"
+//    }
+//}
+
+//subprojects {
+//    println(this.name)
+//    if (this.childProjects.)
+//}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,10 +8,60 @@ pluginManagement {
     }
 }
 
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     versionCatalogs {
         create("commonLibs") {
-            library("paper", "io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+            library("paper", "com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+            library("spigot", "org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
+            library("jedis", "redis.clients:jedis:4.2.3")
+            library("acf", "co.aikar:acf-paper:0.5.0-SNAPSHOT")
+            library("taskchain", "co.aikar:taskchain-bukkit:3.7.2")
+            library("springdatamongodb", "org.springframework.data:spring-data-mongodb:3.4.0")
+            library("mongodbdriversync", "org.mongodb:mongodb-driver-sync:4.6.1")
+            library("mongodbdrivercore", "org.mongodb:mongodb-driver-core:4.6.1")
+            library("placeholderapi", "me.clip:placeholderapi:2.10.3")
+            library("configme", "ch.jalu:configme:1.1.0")
+            library("mythicmobs", "net.elseland.xikage:mythicmobs:4.12.0")
+            library("craftbukkit", "org.bukkit:craftbukkit:1.16.5")
+            library("worldguardevents", "net.raidstone:WorldGuardEvents:1.18.1")
+            library("armorequip", "com.github.Arnuh:ArmorEquipEvent:1.7.1")
+            library("worldguardcore", "com.sk89q.worldguard:worldguard-core:7.0.0-SNAPSHOT")
+            library("worldguardlegacy", "com.sk89q.worldguard:worldguard-legacy:7.0.0-SNAPSHOT")
+            library("holographicdisplays", "com.gmail.filoghost.holographicdisplays:holographicdisplays-api:2.4.0")
+            library("nametagedit", "com.github.sgtcaze:NametagEdit:master-6a2acbd995-1")
+            library("nbtapi", "dr.tr7zw:item-nbt-api:2.7.1")
+            library("tabbed", "com.github.thekeenant:tabbed:v1.8")
+            library("viaversion", "com.github.MylesIsCool.ViaVersion:viaversion:3.2.0")
+            library("protocollib", "com.comphenix.protocol:ProtocolLib:4.5.0")
         }
     }
+    with (repositories) {
+        maven("https://repo.aikar.co/content/groups/aikar/")
+        maven("https://repo.papermc.io/repository/maven-public/")
+        maven("https://repo.dmulloy2.net/nexus/repository/public/")
+        maven("https://repo.aikar.co/content/groups/aikar/")
+        maven("https://maven.enginehub.org/repo/")
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://repo.destroystokyo.com/repository/maven-public/")
+        maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+        maven("https://jitpack.io")
+        maven("https://repo.codemc.io/repository/maven-public/")
+        maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+        flatDir { dirs("libs") }
+        mavenLocal()
+        mavenCentral()
+    }
 }
+include("Projects:Achievements")
+findProject(":Projects:Achievements")?.name = "Achievements"
+include("Projects:Artifacts")
+findProject(":Projects:Artifacts")?.name = "Artifacts"
+include("Projects:Bank")
+findProject(":Projects:Bank")?.name = "Bank"
+include("Projects:Chat")
+findProject(":Projects:Chat")?.name = "Chat"
+include("Projects:Core")
+findProject(":Projects:Core")?.name = "Core"
+include("Projects:FilePull")
+findProject(":Projects:FilePull")?.name = "FilePull"
